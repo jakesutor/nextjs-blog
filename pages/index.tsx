6d3d3cd1c42e50129341b4e4a05c01cb772bb2a8
jakesutor <jake.sutor@gmail.com>
2022-10-18 21:19:54 -0400

    updated blog post

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,10 +51,10 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
       </ul>
 
       <a
-        href="https://github.com/ChangoMan/nextjs-typescript-mdx-blog"
+        href="https://github.com/jakesutor"
         className="inline-block px-7 py-3 rounded-md text-white dark:text-white bg-blue-600 hover:bg-blue-700 hover:text-white dark:hover:text-white"
       >
-        Get the source code!
+        See my GitHub page!
       </a>
 
       {posts.map((post) => (
